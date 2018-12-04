142b54af495c9132c905933558eb1fdfbd151ff0
Glenn Condron <glennc@microsoft.com>
2018-12-04 11:09:32 -0800

    codez

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            //var client = new MyClient("https://localhost:5001", new System.Net.Http.HttpClient());
+            //var s = await client.GetAllAsync();
+        }
+    }
+}
---
+++ WebClientCsharp/Pages/Index.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebClientCsharp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private MyClient _client;
+
+        public IndexModel(MyClient client)
+        {
+            _client = client;
+        }
+
+        public async Task OnGet()
+        {
+            var values = await _client.GetAllAsync();
+        }
+    }
+}
